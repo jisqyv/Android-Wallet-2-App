3f98bb971a055617e43d055230137e3352a01969
WilliamHill <william.hill@gmail.com>
2014-04-30 20:17:32 +0100

    Updated bitmap generator

--- src/info/blockchain/wallet/ui/TxBitmap.java
+++ src/info/blockchain/wallet/ui/TxBitmap.java
@@ -8,9 +8,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Matrix;
-import android.graphics.Typeface;
 import android.content.res.Resources;
-import android.util.Log;
+//import android.util.Log;
 
 public class TxBitmap {
 	
@@ -23,6 +22,8 @@ public class TxBitmap {
 	private static TxBitmap instance = null;
 	
 	private static Context context = null;
+	
+	private static float REG_RES = 2.0f;
 	
 	private TxBitmap() { ; }
 
@@ -73,45 +74,43 @@ public class TxBitmap {
     }
 
     private Bitmap tx_list(int width, int branches) {
-    	
+
+		Resources resources = context.getResources();
+		float scale = resources.getDisplayMetrics().density;
+
     	float fX = 10.0f;
     	float fY = 25.0f;
 		float vOffset = 80.0f;	// down step
-		float vOffsetAmount = 26.0f;
-		float vXtraOffset = 1.0f;
-		float szAddress = 12;
+		float vOffsetAmount = (scale <= REG_RES) ? 26.0f : 32.0f;
+		float vXtraOffset = (scale <= REG_RES) ? 1.0f : 4.0f;
+		float szAddress = 15;
 		float szAmount = 12;
-		
-		Resources resources = context.getResources();
-		float scale = resources.getDisplayMetrics().density;
-		Log.d("Scale", "" + scale);
 
-//    	Bitmap bm = Bitmap.createBitmap(resources.getDisplayMetrics(), (int)(width * scale), (int)(vOffset * branches), Config.ARGB_8888);
     	Bitmap bm = Bitmap.createBitmap(resources.getDisplayMetrics(), width, (int)(vOffset * branches), Config.ARGB_8888);
     	Canvas canvas = new Canvas(bm);
 
     	Paint paintAddressLabel = new Paint(Paint.ANTI_ALIAS_FLAG);
-//    	paintAddressLabel.setTypeface(TypefaceUtil.getInstance(context).getGravityBoldTypeface());
     	paintAddressLabel.setColor(0xff87cefa);
-//		paintAddressLabel.setTextSize((int)(szAddress * scale));
 		paintAddressLabel.setTextSize((int)(szAddress * scale + 0.5f));
-		paintAddressLabel.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+		paintAddressLabel.setTypeface(TypefaceUtil.getInstance(context).getRobotoTypeface());
 
     	Paint paintAddressBTC = new Paint(Paint.ANTI_ALIAS_FLAG);
-//    	paintAddressBTC.setTypeface(TypefaceUtil.getInstance(context).getGravityBoldTypeface());
     	paintAddressBTC.setColor(Color.GRAY);
-//		paintAddressBTC.setTextSize((int)(szAddress * scale));
 		paintAddressBTC.setTextSize((int)(szAddress * scale + 0.5f));
-		paintAddressBTC.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+		paintAddressBTC.setTypeface(TypefaceUtil.getInstance(context).getRobotoTypeface());
 
     	Paint paintAmount = new Paint(Paint.ANTI_ALIAS_FLAG);
-//    	paintAmount.setTypeface(TypefaceUtil.getInstance(context).getGravityBoldTypeface());
     	paintAmount.setColor(Color.GRAY);
-//		paintAmount.setTextSize((int)(szAmount * scale));
 		paintAmount.setTextSize((int)(szAmount * scale + 0.5f));
-		paintAmount.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+		paintAddressLabel.setTypeface(TypefaceUtil.getInstance(context).getRobotoBoldTypeface());
+
+		if(scale <= REG_RES) {
+			canvas.drawText("1HVVe8oF1...", fX, fY + vXtraOffset, paintAddressBTC);
+		}
+		else {
+			canvas.drawText("1HVVe8oF1Bc51234...", fX, fY + vXtraOffset, paintAddressBTC);
+		}
 		
-		canvas.drawText("1HVVe8oF1Bc5...", fX, fY + vXtraOffset, paintAddressBTC);
 		if(branches > 1) {
 			canvas.drawText("0.1020 BTC", fX, fY + vOffsetAmount + vXtraOffset, paintAmount);
 		}
@@ -128,19 +127,17 @@ public class TxBitmap {
     }
 
     private Bitmap tx_arrows(int width, int type, int branches) {
-    	
-    	float fStroke = 4.0f;
-    	float fRadius = 7.0f;	// filled circle at start of line
+
+		Resources resources = context.getResources();
+		float scale = resources.getDisplayMetrics().density;
+
+    	float fStroke = 6.0f;
+    	float fRadius = 8.0f;	// filled circle at start of line
     	float fX = 10.0f;
     	float fY = 20.0f;
 		float hOffset = 20.0f;	// indent
 		float vOffset = 80.0f;	// down step
 
-		Resources resources = context.getResources();
-		float scale = resources.getDisplayMetrics().density;
-		Log.d("Scale", "" + scale);
-
-//    	Bitmap bm = Bitmap.createBitmap(resources.getDisplayMetrics(), (int)(width * scale), (int)(vOffset * branches), Config.ARGB_8888);
     	Bitmap bm = Bitmap.createBitmap(resources.getDisplayMetrics(), width, (int)(vOffset * branches), Config.ARGB_8888);
     	Canvas canvas = new Canvas(bm);
     	
@@ -149,14 +146,14 @@ public class TxBitmap {
     	paint.setStrokeWidth(fStroke);
     	canvas.drawCircle(fX, fY, fRadius, paint);
     	canvas.drawLine(fX, fY, canvas.getWidth() - fX, fY, paint);
-		doArrowhead(canvas, fX, fY, canvas.getWidth() - fX, fY, type);
+		doArrowhead(canvas, fX + 3.0f, fY, (canvas.getWidth() - fX) + 3.0f, fY, type);
 		
     	if(branches > 1) {
     		int remaining_branches = branches - 1;
     		for(int i = 0; i < remaining_branches; i++) {
             	canvas.drawLine(fX + hOffset, fY, fX + hOffset, fY + (vOffset * (i + 1)), paint);
             	canvas.drawLine(fX + hOffset, fY + (vOffset * (i + 1)), canvas.getWidth() - fX, fY + (vOffset * (i + 1)), paint);
-            	doArrowhead(canvas, fX + hOffset, fY + (vOffset * (i + 1)), canvas.getWidth() - fX, fY + (vOffset * (i + 1)), type);
+            	doArrowhead(canvas, fX + hOffset + 3.0f, fY + (vOffset * (i + 1)), (canvas.getWidth() - fX) + 3.0f, fY + (vOffset * (i + 1)), type);
     		}
     	}
 
@@ -164,14 +161,14 @@ public class TxBitmap {
     }
 
     private void doArrowhead(Canvas canvas, float x0, float y0, float x1, float y1, int type) {
-
+    	
     	Paint paint = new Paint();
     	paint.setColor(type == SENDING ? BlockchainUtil.BLOCKCHAIN_RED : BlockchainUtil.BLOCKCHAIN_GREEN);
         paint.setStyle(Paint.Style.FILL);
 
         float deltaX = x1 - x0;
         float deltaY = y1 - y0;
-        float frac = 0.045f;
+        float frac = 0.065f;	// arrow head size
 
         float point_x_1 = x0 + ((1.0f - frac) * deltaX + frac * deltaY);
         float point_y_1 = y0 + ((1.0f - frac) * deltaY - frac * deltaX);
