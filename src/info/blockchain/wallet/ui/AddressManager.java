8be398a0cfafedc36a8d56f3c2437f2ac999fb76
Tim Lee <tim@blockchain.info>
2014-05-29 13:03:45 -0700

    newAddress

--- src/info/blockchain/wallet/ui/AddressManager.java
+++ src/info/blockchain/wallet/ui/AddressManager.java
@@ -64,4 +64,75 @@ public class AddressManager {
 			}
 		});
 	}
+	
+	public void newAddress(final AddAddressCallback callback) {
+		final ECKey key = application.getRemoteWallet().generateECKey();			
+		final String address = key.toAddress(NetworkParameters.prodNet()).toString();
+		final String label = null;
+		
+		if (blockchainWallet == null) {
+			callback.onError("Wallet null.");
+			return;
+		}
+
+		if (application.isInP2PFallbackMode()) {
+			callback.onError("Error saving wallet.");
+			return;
+		}
+
+		try {
+			final boolean success = blockchainWallet.addKey(key, address, label);
+			if (success) {
+				application.localSaveWallet();
+
+				application.saveWallet(new SuccessCallback() {
+					@Override
+					public void onSuccess() {
+						application.checkIfWalletHasUpdated(blockchainWallet.getTemporyPassword(), false, new SuccessCallback() {
+							@Override
+							public void onSuccess() {	
+								try {
+									ECKey key = blockchainWallet.getECKey(address);									
+									if (key != null && key.toAddress(NetworkParameters.prodNet()).toString().equals(address)) {
+										callback.onSavedAddress(address);
+									} else {
+										blockchainWallet.removeKey(key);
+
+										callback.onError("WARNING! Wallet saved but address doesn't seem to exist after re-read.");
+									}
+								} catch (Exception e) {
+									blockchainWallet.removeKey(key);
+
+									callback.onError("WARNING! Error checking if ECKey is valid on re-read.");
+								}
+							}
+
+							@Override
+							public void onFail() {
+								blockchainWallet.removeKey(key);
+
+								callback.onError("WARNING! Error checking if address was correctly saved.");
+							}
+						});
+					}
+
+					@Override
+					public void onFail() {
+						blockchainWallet.removeKey(key);
+
+						callback.onError("Error saving wallet");
+					}
+				});
+			} else {
+				callback.onError("addKey returned false");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			application.writeException(e);
+
+			callback.onError(e.getLocalizedMessage());
+		}
+	}
 }
