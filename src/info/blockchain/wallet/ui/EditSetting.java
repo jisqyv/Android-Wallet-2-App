394e3c0e257915d3b6e19ca478b06cb42ef869e3
Tim Lee <tim@blockchain.info>
2014-07-24 10:56:07 -0700

    Password Creation - Keyboard automatically activated

--- src/info/blockchain/wallet/ui/EditSetting.java
+++ src/info/blockchain/wallet/ui/EditSetting.java
@@ -2,12 +2,14 @@ package info.blockchain.wallet.ui;
 
 import piuk.blockchain.android.R;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -67,6 +69,14 @@ public class EditSetting extends Activity {
             	finish();
             }
        });
+       
+       etValue.postDelayed(new Runnable() {
+    	   @Override
+    	   public void run() {
+    	     InputMethodManager keyboard = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+    	     keyboard.showSoftInput(etValue, 0);
+    	   }
+  	   }, 50);
     }
     
 	@Override
